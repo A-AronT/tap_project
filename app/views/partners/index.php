5f811f3a5c837e5bb613975fd58a7883059b8f53
Aron Truu <saararon@gmail.com>
2021-05-05 11:02:21 +0300

    Showed partners model data at all partners view

--- app/views/partners/index.php
+++ app/views/partners/index.php
@@ -1,8 +1,4 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
-<?php
-echo '<pre>';
-print_r($data);
-?>
     <h1><?php echo $data['title']; ?></h1>
     <!-- table -->
     <table class="table table-striped">
@@ -19,56 +15,18 @@ print_r($data);
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Quinton</td>
-            <td>12345</td>
-            <td>quin71@gmail.com</td>
-            <td>1234567</td>
-            <td>basseinid</td>
-            <td>kloriini käsitlus</td>
-            <td>uus meremaa</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Toomas Peeterson</td>
-            <td>23456</td>
-            <td>toomasp@gmail.com</td>
-            <td>2345678</td>
-            <td>katused</td>
-            <td>plekkkatused</td>
-            <td>üle eesti</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Toomas Peeterson</td>
-            <td>23456</td>
-            <td>toomasp@gmail.com</td>
-            <td>2345678</td>
-            <td>katused</td>
-            <td>puitkatused</td>
-            <td>üle eesti</td>
-        </tr>
-        <tr>
-            <th scope="row">4</th>
-            <td>Juhan</td>
-            <td>52312</td>
-            <td>jupats@gmail.com</td>
-            <td>3456789</td>
-            <td>põrandad</td>
-            <td>puitpõrandad</td>
-            <td>tartumaa</td>
-        </tr>
-        <tr>
-            <th scope="row">5</th>
-            <td>Jüri</td>
-            <td>63136</td>
-            <td>ratas@eesti.ee</td>
-            <td>4567891</td>
-            <td>maja ehitus</td>
-            <td>palkmajad</td>
-            <td>üle eesti</td>
-        </tr>
+        <?php foreach ($data['partners'] as $partner): ?>
+            <tr>
+                <th scope="row"><?php echo $partner->id; ?></th>
+                <td><?php echo $partner->name; ?></td>
+                <td><?php echo $partner->reg_nr; ?></td>
+                <td><?php echo $partner->email; ?></td>
+                <td><?php echo $partner->phone; ?></td>
+                <td><?php echo $partner->activity; ?></td>
+                <td><?php echo $partner->activity_details; ?></td>
+                <td><?php echo $partner->location; ?></td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
     <!-- table -->
