692dee17b0bbacf5136ddaeef9af1241661f9605
Aron Truu <saararon@gmail.com>
2021-05-26 11:15:52 +0300

    refactored navbar structure

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo PROJECT_NAME; ?></a>
+        <a class="navbar-brand" href="<?php echo URLROOT; ?>/partners/index"><?php echo PROJECT_NAME; ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/partners/index">Koostööpartnerid</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/activities/index">Teenused</a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
